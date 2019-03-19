5ec540e937ac3ee211b3fb2aceec49a464b907a9
Gabriel Huhn <4rchitect.h@gmail.com>
2019-03-19 14:55:02 -0300

    Add PlayerEnergy script

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnergy : MonoBehaviour
+{
+    [SerializeField]
+    private float initialEnergy = 3f;
+    [SerializeField]
+    private float regenSpeed = 1f;
+    private float currentEnergy;
+
+    // TODO: Work on the HUD and do the operations.
+
+    void Awake()
+    {
+        currentEnergy = initialEnergy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentEnergy += regenSpeed * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Verify if the player has energy to perform a hit.
+    /// </summary>
+    /// <param name="energyRequired"></param>
+    /// <returns>Returns true, false otherwise.</returns>
+    protected bool CanCast(float energyRequired)
+    {
+        if (currentEnergy >= energyRequired)
+        {
+            currentEnergy -= energyRequired;
+            return true;
+        }
+        else
+            return false;
+    }
+}
