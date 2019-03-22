c69cf7a5b191812fbcc5158174743e8c4c607dc4
Will Saymon <wsaymonoficial@gmail.com>
2019-03-22 10:55:39 -0300

    Working Main Menu + more better animations time

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public Animator canvasAnimator;
+
+    public void ShowControls(){
+        canvasAnimator.SetBool("Controls", true);
+    }
+
+    public void ShowCredits(){
+        canvasAnimator.SetBool("Credits", true);
+    }
+
+    public void ShowMenu(){
+        canvasAnimator.SetBool("Controls", false);
+        canvasAnimator.SetBool("Credits", false);
+    }
+}
